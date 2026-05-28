e695169a8a8d16acb4207c7ca3faaed203a81909
vinay.betha <vinay.betha@techvedika.com>
2026-05-28 23:42:53 +0530

    Update in cors

--- Backend/BlogBase/src/main/java/com/blog/config/CorsConfig.java
+++ Backend/BlogBase/src/main/java/com/blog/config/CorsConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
-    @Value("${app.cors.allowed-origins:https://blog-base-git-grogufrontend-suryavinay-livecoms-projects.vercel.app,https://blog-base-neon.vercel.app,http://localhost:5173,https://localhost:5173,https://api.blogbase.com,http://api.blogbase.com}")
+    @Value("${app.cors.allowed-origins:https://blogbase.suryavinaybetha.xyz,https://blog-base-git-grogufrontend-suryavinay-livecoms-projects.vercel.app,https://blog-base-neon.vercel.app,http://localhost:5173,https://localhost:5173,https://api.blogbase.com,http://api.blogbase.com}")
     private String[] allowedOrigins;
 
     @Override
